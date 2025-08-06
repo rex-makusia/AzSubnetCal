564835e110777598061b4acd4a86390d5126548a
Rex Makusia <RMakusia@bsp.com.pg>
2025-08-07 07:59:02 +1000

    Create the started program files

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"flag" // For parsing command-line flags
+	"fmt"  // For printing output
+	"math" // For calculations like log2
+	"net"  // For CIDR parsing and IP math
+	"os"   // For exiting on error
+)
+
+func main() {
+	
+}
